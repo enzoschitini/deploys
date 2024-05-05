6acffb23857a30d6c5a31bbda9aa5ab0d712d998
Enzo Schitini <70824630+enzoschitini@users.noreply.github.com>
2024-05-05 17:44:49 -0300

    .

---
+++ Decision Tree Classifier/da.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(layout='wide', page_title='Da Vinci',
+                   page_icon="./Files/Vector.png")
+
+st.sidebar.image("./Files/Nome.png", width=200)
+
+st.write('Ok')  

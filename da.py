3647458cb2356f89ea12e80bb4bec8ff63443b6b
Enzo Schitini <70824630+enzoschitini@users.noreply.github.com>
2024-05-06 21:04:15 -0300

    live

--- da.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.set_page_config(layout='wide', page_title='Da Vinci',
-                   page_icon="./Files/Vector.png")
-
-st.sidebar.image("./Files/Nome.png", width=200)
-
-st.write('Ok')  

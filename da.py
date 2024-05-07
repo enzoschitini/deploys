ac99ffe6109be5c46fae753a3c9c72dd34f078fa
Enzo Schitini <70824630+enzoschitini@users.noreply.github.com>
2024-05-06 21:33:21 -0300

    Live

--- da.py
+++ da.py
@@ -26,6 +26,7 @@ st.set_page_config(layout='wide', page_title='Da Vinci',
 
 st.image("./Files/Nome.png", width=250)
 st.title('Benvenuto a Streamlit!')  
+st.write('---')
 
 st.sidebar.write('Bar')
 
@@ -186,4 +187,66 @@ elif pagina_impostata == 'Collegamento tra le variabili':
                 st.pyplot(fig)
 
 elif pagina_impostata == 'Tabella Esplorativa':
+            
+            def guida(data_frame): # 14/02/2024
+                righe, qnt_colonne = data_frame.shape
+                quantita_righe = format(righe, ",").replace(',', '.')
+                sequenza = list(range(qnt_colonne + 1))
+                sequenza = sequenza[1:]
+
+                colonne = data_frame.columns.to_list()
+                types_list = [str(type(data_frame[col][0])).split("'")[1] for col in data_frame.columns]
+                lista_categorie = [data_frame[col].nunique() for col in data_frame.columns]
+
+                elementi_nulli = data_frame.isnull().sum()
+                elementi_nulli = elementi_nulli.to_list()
+
+                memory = (data_frame.memory_usage(deep=True) / (1024 ** 2)).round() # Mb
+                memory_list = memory.to_list()
+                memory_list = memory_list[1:]
+
+                memory = data_frame.memory_usage(deep=True) # Totale Mb
+                memory_totale = round(memory.sum() / (1024 ** 2), 2)
+
+                percentuale_dati_nulli = round((data_frame.isnull().sum() / righe) * 100)
+                percentuale_dati_nulli = percentuale_dati_nulli.to_list()
+
+                data = pd.DataFrame({'Nome': colonne, 
+                                    'Tipo': types_list, 
+                                    'qunt_categorie': lista_categorie,
+                                    'Dati nulli' : elementi_nulli,
+                                    'Dati nulli %' : percentuale_dati_nulli,
+                                    'Memoria (Mb)': memory_list}, index=sequenza)
+                
+                # Intestazioni
+                print('Teabella Esplorativa')
+                print(f'In questi dati abbiamo {quantita_righe} righe e {qnt_colonne} colonne.')
+                print(f'Consumassione di memoria: {memory_totale}Mb.')
+                
+                return data
+
             st.write('Va bene!!!')
+        
+            uploaded_file = st.file_uploader("Carica un set di dati da analizzare", type=["csv"])
+
+            if uploaded_file:
+                data = pd.read_csv(uploaded_file)
+                
+                with st.expander("Vedere i dati"):
+                    st.write(data)
+                    righe, colonne = data.shape
+                    st.write("Ci sono " + str(righe) + ' righe e ' + str(colonne) + ' colonne')
+            
+            #tipo_impostato = st.selectbox("  ", tipo_delle_colonne)
+
+            if uploaded_file:
+                start_loading = st.button("Avviare il processo")
+
+                if start_loading:
+                    progress_bar = st.progress(0)
+
+                    for percent_complete in simulated_process():
+                        progress_bar.progress(percent_complete)
+
+                        tabella = guida(data)
+                        st.write(tabella)

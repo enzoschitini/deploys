cc03be0b9ca92ed5a98d7a464e69ec6cf5600d3b
Enzo Schitini <70824630+enzoschitini@users.noreply.github.com>
2024-05-08 21:15:47 -0300

    .

--- algoritmo.py
+++ algoritmo.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 class Avvio(object):
     def __init__(self) -> None:
@@ -6,3 +7,40 @@ class Avvio(object):
 
     def benvenuto(self):  # Add the 'self' parameter here
         st.write('Benvenuto')
+    
+    def guida(data_frame): # 14/02/2024
+        righe, qnt_colonne = data_frame.shape
+        quantita_righe = format(righe, ",").replace(',', '.')
+        sequenza = list(range(qnt_colonne + 1))
+        sequenza = sequenza[1:]
+
+        colonne = data_frame.columns.to_list()
+        types_list = [str(type(data_frame[col][0])).split("'")[1] for col in data_frame.columns]
+        lista_categorie = [data_frame[col].nunique() for col in data_frame.columns]
+
+        elementi_nulli = data_frame.isnull().sum()
+        elementi_nulli = elementi_nulli.to_list()
+
+        memory = (data_frame.memory_usage(deep=True) / (1024 ** 2)).round() # Mb
+        memory_list = memory.to_list()
+        memory_list = memory_list[1:]
+
+        memory = data_frame.memory_usage(deep=True) # Totale Mb
+        memory_totale = round(memory.sum() / (1024 ** 2), 2)
+
+        percentuale_dati_nulli = round((data_frame.isnull().sum() / righe) * 100)
+        percentuale_dati_nulli = percentuale_dati_nulli.to_list()
+
+        data = pd.DataFrame({'Nome': colonne, 
+                            'Tipo': types_list, 
+                            'qunt_categorie': lista_categorie,
+                            'Dati nulli' : elementi_nulli,
+                            'Dati nulli %' : percentuale_dati_nulli,
+                            'Memoria (Mb)': memory_list}, index=sequenza)
+        
+        # Intestazioni
+        print('Teabella Esplorativa')
+        print(f'In questi dati abbiamo {quantita_righe} righe e {qnt_colonne} colonne.')
+        print(f'Consumassione di memoria: {memory_totale}Mb.')
+        
+        return data
--- da.py
+++ da.py
@@ -194,43 +194,6 @@ elif pagina_impostata == 'Collegamento tra le variabili':
                 st.pyplot(fig)
 
 elif pagina_impostata == 'Tabella Esplorativa':
-            
-            def guida(data_frame): # 14/02/2024
-                righe, qnt_colonne = data_frame.shape
-                quantita_righe = format(righe, ",").replace(',', '.')
-                sequenza = list(range(qnt_colonne + 1))
-                sequenza = sequenza[1:]
-
-                colonne = data_frame.columns.to_list()
-                types_list = [str(type(data_frame[col][0])).split("'")[1] for col in data_frame.columns]
-                lista_categorie = [data_frame[col].nunique() for col in data_frame.columns]
-
-                elementi_nulli = data_frame.isnull().sum()
-                elementi_nulli = elementi_nulli.to_list()
-
-                memory = (data_frame.memory_usage(deep=True) / (1024 ** 2)).round() # Mb
-                memory_list = memory.to_list()
-                memory_list = memory_list[1:]
-
-                memory = data_frame.memory_usage(deep=True) # Totale Mb
-                memory_totale = round(memory.sum() / (1024 ** 2), 2)
-
-                percentuale_dati_nulli = round((data_frame.isnull().sum() / righe) * 100)
-                percentuale_dati_nulli = percentuale_dati_nulli.to_list()
-
-                data = pd.DataFrame({'Nome': colonne, 
-                                    'Tipo': types_list, 
-                                    'qunt_categorie': lista_categorie,
-                                    'Dati nulli' : elementi_nulli,
-                                    'Dati nulli %' : percentuale_dati_nulli,
-                                    'Memoria (Mb)': memory_list}, index=sequenza)
-                
-                # Intestazioni
-                print('Teabella Esplorativa')
-                print(f'In questi dati abbiamo {quantita_righe} righe e {qnt_colonne} colonne.')
-                print(f'Consumassione di memoria: {memory_totale}Mb.')
-                
-                return data
 
             st.write('Va bene!!!')
         

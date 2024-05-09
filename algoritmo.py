b679baf76315e470b564c50fda3b11ecab518fec
Enzo Schitini <70824630+enzoschitini@users.noreply.github.com>
2024-05-08 21:10:35 -0300

    Da Vinci

---
+++ algoritmo.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+class Avvio(object):
+    def __init__(self) -> None:
+        pass
+
+    def benvenuto(self):  # Add the 'self' parameter here
+        st.write('Benvenuto')
